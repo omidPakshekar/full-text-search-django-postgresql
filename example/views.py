ae4cdcd17e17ea7ef2d7cd42d91004a74fbc806a
omid paa <omid.pakshekar.est@gmail.com>
2023-02-17 14:27:16 +0330

    basic search

--- example/views.py
+++ example/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Video
 
-# Create your views here.
+
+def index(request):
+    q = request.GET.get('q') 
+
+    videos = Video.objects.all()
+    if q:
+        videos = Video.objects.filter(title_search=q)
+        
+    return render(request, 'example/index.html', {'videos' : videos})
+
+
+
+
+
+
+
+
